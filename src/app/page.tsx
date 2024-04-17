4dfc13e978759bc469bda35984615b589b771702
Lawrence <lawrence.cuenco@sjsu.edu>
2024-04-16 21:14:40 -0700

    Added weather forecast for temperature, weather icons, and day names

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,16 +26,45 @@ const fetcher = async (url: string): Promise<any> => {
 };
 
 
+
 export default function Home() {
   const [city, setCity] = useState('San Jose'); // Default city
   const apiKey = process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY;// Replace with your OpenWeatherMap API key
   const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`; 
+  const futureApiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`
 
   const { data, error } = useSWR(apiUrl, fetcher);
+  const future = useSWR(futureApiUrl, fetcher);
+  const forecastdata = future.data
 
   if (error) return <div>Error: {error.message}</div>;
   if (!data) return <div>Loading...</div>;
+  if (!forecastdata) return <div>Loading...</div>;
   console.log(data)
+  console.log(forecastdata)
+  
+  function tempForecast (params : number) {
+    var max = 0;
+      for (var i = 0; i < 8; i++) {
+        var g = i + (params * 8)
+        if (forecastdata.list[g].main.temp > max) {
+          max = forecastdata.list[g].main.temp
+        }
+      }
+    return Math.round(max)
+  }
+
+  function weatherForecast (params : number) {
+    var code = forecastdata.list[params].weather[0].icon
+    var imgurl = `http://openweathermap.org/img/w/${code}.png`
+    return imgurl
+  }
+
+  function dayName (date : string) {
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    var d = new Date(date);
+    return days[d.getDay()];
+  }
 
   return (
     <main>
@@ -58,31 +87,29 @@ export default function Home() {
             <CardFooter>
               <div className="grid grid-rows-5 gap-4">
                 <div className="grid grid-cols-3 gap-80">
-                  <p>Monday</p>
-                  <p>Sunny</p>
-                  <p>24°C</p>
+                    <p>{dayName(forecastdata.list[0].dt_txt)}</p>
+                    <img src={weatherForecast(6)}/>
+                    <p>{tempForecast(0)}°</p>
                 </div>
                 <div className="grid grid-cols-3 gap-80">
-                  <p>Tuesday</p>
-                  <p>Cloudy</p>
-                  <p>20°C</p>
+                  <p>{dayName(forecastdata.list[8].dt_txt)}</p>
+                  <img src={weatherForecast(14)}/>
+                  <p>{tempForecast(1)}°</p>
                 </div>
                 <div className="grid grid-cols-3 gap-80">
-                  <p>Wednesday</p>
-                  <p>Rainy</p>
-                  <p>18°C</p>
+                  <p>{dayName(forecastdata.list[16].dt_txt)}</p>
+                  <img src={weatherForecast(22)}/>
+                  <p>{tempForecast(2)}°</p>
                 </div>
                 <div className="grid grid-cols-3 gap-80">
-                  <p>Thursday</p>
-                  <Avatar>
-                    <AvatarImage src="/images/sun.png" />
-                  </Avatar>
-                  <p>70°C</p>
+                  <p>{dayName(forecastdata.list[24].dt_txt)}</p>
+                  <img src={weatherForecast(30)}/>
+                  <p>{tempForecast(3)}°</p>
                 </div>
                 <div className="grid grid-cols-3 gap-80">
-                  <p>Friday</p>
-                  <p>Partly</p>
-                  <p>22°C</p>
+                  <p>{dayName(forecastdata.list[32].dt_txt)}</p>
+                  <img src={weatherForecast(38)}/>
+                  <p>{tempForecast(4)}°</p>
                 </div>
               </div>
             </CardFooter>
